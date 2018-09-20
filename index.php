673abd12bfe02828390dd61330ebedaa30ae6a69
Misthaayu <43429796+Misthaayu@users.noreply.github.com>
2018-09-20 13:03:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<html>
+	<title>Biodata</title>
+	<body>
+
+		<?php 
+echo "<font color= black face='Comic sans ms'>";
+
+echo "Hello World, My Name is Mistha Ayu Wardoyo";
+
+echo "<br>";
+echo "<br>";
+
+echo "<B>";
+echo "My Biodata";
+echo "<br>";
+echo "<br>";
+
+echo "<table width='560' border='0'>";
+  echo "<tr>";
+    echo "<td width='51'>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td width='227'>";
+        echo "Nama Lengkap";
+        echo "</td>";
+    echo "<td width='8'>";
+        echo ":";
+        echo "</td>";
+    echo "<td width='254'>";
+        echo "Mistha Ayu Wardoyo";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Nama Panggilan";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Mistha";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+       
+    echo "<td>";
+        echo "Tempat, Tanggal Lahir";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Ponorogo, 22 Januari 1999";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Kuliah";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "STMIK AKAKOM YOGYAKARTA";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+       
+    echo "<td>";
+        echo "Alamat";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Jl.Janti Gg.onotoseno No.126A";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+       
+    echo "<td>";
+        echo "Tinggi/ Berat";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "170/ 60";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+       
+    echo "<td>";
+        echo "Cita-cita";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Bimbang";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Agama";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Islam ";
+        echo "</td>";
+  echo "</tr>";
+echo "</table>";
+?>
+	</body>
+</html>
